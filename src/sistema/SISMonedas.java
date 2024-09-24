6b0394bb60076ae50a93277fe6b423360bc120de
portuking <dopazopablo2004@gmail.com>
2024-09-24 16:53:00 +0200

    sistema de monedas y atributos de la clase pez

--- src/sistema/SISMonedas.java
+++ src/sistema/SISMonedas.java
@@ -1,5 +1,14 @@
 package sistema;
 
 public class SISMonedas {
-    
+    private int coins;
+
+    public void setCoins(int nCoins) {
+        coins += nCoins;
+    }
+
+    public int getCoins(){
+        return coins;
+    }
+
 }
