6b4a1e71fb14111e25905b07d5f7889b770a3333
pdopazo <dopazopablo2004@gmail.com>
2024-10-21 21:41:30 +0200

    Arreglos en Pez.java e implementación de AlimentacionCarnivoro.java

--- src/peces/alimentacion/AlimentacionCarnivoro.java
+++ src/peces/alimentacion/AlimentacionCarnivoro.java
@@ -1,5 +1,38 @@
 package peces.alimentacion;
 
-public class AlimentacionCarnivoro {
+import edificios.tanque.Tanque;
+import peces.Pez;
+import propiedades.PecesDatos;
+import java.util.Random;
+
+public class AlimentacionCarnivoro extends Pez{
+
+    public AlimentacionCarnivoro(PecesDatos fishStats, boolean sex){
+        super(sex, fishStats);
+    }
+
+    @Override
+    public void eat(Tanque<? extends Pez> tank) {
+        for (int i = 0; i < tank.getFishes().size(); i++) {
+            if(tank.getFishes().get(i) != null) {
+                if(!tank.getFishes().get(i).isAlive()) {
+                    this.setHungry(false);
+                    Random r = new Random();
+                    boolean random = r.nextBoolean();
+                    if(random == false) {
+                        tank.getFishes().set(i, null);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public Pez getInstance() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInstance'");
+    }
+
 
 }
