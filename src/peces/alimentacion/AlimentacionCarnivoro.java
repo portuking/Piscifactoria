b8371480745b2c882abef145d5217aeda4528e0e
AdrianCes <adriances14@gmail.com>
2024-10-29 16:41:49 +0100

    Javadoc de alimentaciones

--- src/peces/alimentacion/AlimentacionCarnivoro.java
+++ src/peces/alimentacion/AlimentacionCarnivoro.java
@@ -3,12 +3,29 @@ package peces.alimentacion;
 import peces.Pez;
 import propiedades.PecesDatos;
 
+/**
+ * Clase que representa la alimentación carnívora de un pez
+ * @author Manuel Abalo Rietz
+ * @author Adrián Ces López
+ * @author Pablo Dopazo Suárez
+ */
 public class AlimentacionCarnivoro extends Pez {
 
+    /**
+     * Constructor para crear un nuevo pez carnívoro.
+     *
+     * @param fishStats Objeto de tipo PecesDatos que contiene las estadísticas del pez.
+     * @param sex       booleano que indica el sexo del pez (true si es macho, false si es hembra).
+     */
     public AlimentacionCarnivoro(PecesDatos fishStats, boolean sex) {
         super(sex, fishStats);
     }
 
+     /**
+     * Método que simula la acción de comer del pez carnívoro.
+     *
+     * @return La cantidad de comida consumida (1 si ha comido).
+     */
     @Override
     public int eat() {
         int comidaConsumida = 0;
@@ -19,6 +36,12 @@ public class AlimentacionCarnivoro extends Pez {
         return comidaConsumida;
     }
 
+    /**
+     * Método que crea un nuevo pez. Este método no está implementado y lanzará una excepción.
+     *
+     * @return Un nuevo objeto de tipo Pez (no implementado).
+     * @throws UnsupportedOperationException Si se llama a este método.
+     */
     @Override
     public Pez getNewFish() {
         throw new UnsupportedOperationException("Método sin implementar");
