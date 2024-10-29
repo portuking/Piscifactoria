45e8eb07416330e839a44c3b49f9355d039a092d
portuking <dopazopablo2004@gmail.com>
2024-10-29 02:08:02 +0100

    Arreglos clase Tanque.java

--- src/peces/alimentacion/AlimentacionFiltrador.java
+++ src/peces/alimentacion/AlimentacionFiltrador.java
@@ -6,29 +6,28 @@ import propiedades.PecesDatos;
 
 public class AlimentacionFiltrador extends Pez {
 
-    public AlimentacionFiltrador(PecesDatos fishStats, boolean sex){
+    public AlimentacionFiltrador(PecesDatos fishStats, boolean sex) {
         super(sex, fishStats);
     }
 
     @Override
-    public Pez getInstance() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getInstance'");
+    public Pez getNewFish() {
+        throw new UnsupportedOperationException("Método no Implementado");
     }
 
     @Override
     public int eat() {
-        int comidaConsumida= 0;
-            if(this.isAlive() && !this.isEat()) {
-                    this.eat = true;
-                    Random r = new Random();
-                    if(r.nextBoolean()){
-                        comidaConsumida = 0;
-                    }else{
-                        comidaConsumida = 1;
-                    }
-                }  
-                return comidaConsumida;
+        int comidaConsumida = 0;
+        if (this.isAlive() && !this.isEat()) {
+            this.eat = true;
+            Random r = new Random();
+            if (r.nextBoolean()) {
+                comidaConsumida = 0;
+            } else {
+                comidaConsumida = 1;
             }
+        }
+        return comidaConsumida;
+    }
 
 }
